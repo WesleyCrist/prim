63abeff8439285c9ad26624aec3097607c8bb046
WesleyCrist <wesleycristiano@outlook.com>
2023-05-21 21:54:07 -0300

    aviso para rota não encontrada configurada na aplicação.

--- src/routes/grouping.ts
+++ src/routes/grouping.ts
@@ -1,9 +1,11 @@
 import Elysia from "elysia"
 import init from "./init.routes"
 import prim from "./prim.routes"
+import none from "./none.routes"
 
 const grouping = (app: Elysia) => app
     .use(init)
     .use(prim)
+    .use(none)
 
 export default grouping
--- src/routes/none.routes.ts
+++ src/routes/none.routes.ts
@@ -1,1 +1,14 @@
-// em construção
+import Elysia from "elysia"
+
+const none = (app: Elysia) => app
+    .onError(({ code, error, set }) => {
+        if(code === 'NOT_FOUND') {
+            set.status = 404
+            return {
+                statusCode: 404,
+                statusApplication: 'Page not found'
+            }
+        }
+    })
+
+export default none
